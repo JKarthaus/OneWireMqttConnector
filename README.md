b3223020146378499cdb10a6423ade9f651a5e84
jkarthaus <joern.karthaus@gmail.com>
2015-10-06 22:23:10 +0200

    Implementing the Testing features Update some Hints in README

--- README.md
+++ README.md
@@ -2,5 +2,20 @@
 
 Send Data from OneWireSensors to an MQTT Broker
 
+### Commands
 
 * sudo update-rc.d /etc/init.d/onewiremqttd defaults
+
+* sudo service onewiremqttd start
+
+* sudo service onewiremqttd stop
+
+* sudo service onewiremqttd status
+
+### For testing and debugging
+
+* sudo service onewiremqttd stop
+
+* start debug_onewiremqttd
+
+* edit debug_onewiremqttd_data.sh
---
+++ bin/debug_onewiremqttd_data.sh
@@ -0,0 +1,23 @@
+#############################################
+# Edit this Variables to send Dummy Data
+# to HeatingControl2
+
+# 500 Liter Pufferspeicher
+ID_1=22-000000336328
+VALUE_1=66
+
+# Sensor Ruecklauf Solarkreislauf
+ID_2=28-0214635c11ff
+VALUE_2=21
+
+# Sensor Vorlauf Brauchwasser
+ID_3=28-0214631f29ff
+VALUE_3=33
+
+# Sensor Brauchwassertank
+ID_4=28-0214631080ff
+VALUE_4=43
+
+# Sensor Vorlauf Heizkreislauf
+ID_5=28-0214633b64ff
+VALUE_5=22
